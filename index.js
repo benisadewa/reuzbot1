df5422086af694f379ddb4c60a851341a943e33c
benisadewa <73853490+benisadewa@users.noreply.github.com>
2020-11-04 18:08:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const menu = require("./lib/menu.js");
 const donate = require("./lib/donate.js");
 const info = require("./lib/info.js");
 //
-const BotName = 'ST4RZ BOT 🤖'; // Nama Bot Whatsapp
+const BotName = 'REUZ BOT'; // Nama Bot Whatsapp
 const instagramlu = 'https://instagram.com/reuz.id'; // Nama Instagramlu cok
 const whatsapplu = '081271699856'; // Nomor whatsapplu cok
 const kapanbotaktif = '24 Jam'; // Kapan bot lu aktif
-const grupch1 = 'https://chat.whatsapp.com/FsAlnxqz6y2BhCQi5ayCLG'; // OFFICIAL GRUP LU 1
-const grupch2 = 'https://chat.whatsapp.com/KLW3UlFfeaH36Ucm5zRfCz'; // OFFICIAL GRUP LU 2
+const
+
 //
 const
 {
